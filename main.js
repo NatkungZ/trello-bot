762573d52a3aa736ac0f3e5bdc77a340dc33cedf
Supakorn Wrd <supakorn@proteus-tech.com>
2015-08-12 00:04:56 +0700

    Add support slash

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ login({email: process.env.FB_LOGIN_EMAIL, password: process.env.FB_PASSWORD}, fu
         bot_send_msg(api,msg,ziko);
 
       else if(user=="thoth")
-        bot_send_msg(api,msg,projectGroup);
+        bot_send_msg(api,request.url.substring(7),projectGroup);
     } catch(err) {
       console.log(err);
     }
